04b4edd6b6272a7a83e08266170baea991b6e708
David Formánek <->
2013-03-06 22:32:10 +0100

    Added javadoc to parser methods

--- src/org/cesta/parsers/java/IntegrityVariablesMembers.java
+++ src/org/cesta/parsers/java/IntegrityVariablesMembers.java
@@ -22,8 +22,10 @@ public abstract class IntegrityVariablesMembers extends AbstractTreeParser {
     
     // parser members
     
-    // TODO: add Javadoc
     
+    /**
+     * Variable representation
+     */
     public class Variable {
         private String name;
         private String type;
@@ -46,6 +48,13 @@ public abstract class IntegrityVariablesMembers extends AbstractTreeParser {
             return name;
         }
 
+        /**
+         * Determine whether the variable represented by this instance is one
+         * of the variables we want to transform (thus we can do it and want
+         * to do it).
+         * 
+         * @return true if should, false otherwise
+         */
         public boolean shouldBeTransformed() {
             // TODO: more considerations
             return (type != null) && isSupportedType();
@@ -61,6 +70,11 @@ public abstract class IntegrityVariablesMembers extends AbstractTreeParser {
         return type.equals("boolean") || type.equals("byte");
     }
 
+    /**
+     * Adds imports specified in "additionalImports" template.
+     * 
+     * @param tree place after the added imports
+     */
     public void addImports(CommonTree tree) {
             if (tree != null) {
                 getLogger().finer("Adding additional imports");
@@ -69,6 +83,11 @@ public abstract class IntegrityVariablesMembers extends AbstractTreeParser {
             }
     }
 
+    /**
+     * Adds special methods from templates to the class.
+     * 
+     * @param tree place before the added code
+     */
     public void addCode(CommonTree tree) {
         // TODO: mask randomization
         StringTemplate st;
@@ -94,6 +113,14 @@ public abstract class IntegrityVariablesMembers extends AbstractTreeParser {
         // TODO: protection of a type short
     }
 
+    /**
+     * Replaces variable type in a declaration of a protected variable
+     * according to the template.
+     * 
+     * @param tree tree with the old type
+     * @param type the old type
+     * @return true whether the type changed, false otherwise
+     */
     public boolean replaceType(CommonTree tree, String type) {
         // TODO: check variables instead of a type only
         if (!isSupportedType(type)) {
@@ -104,6 +131,17 @@ public abstract class IntegrityVariablesMembers extends AbstractTreeParser {
         return true;
     }
 
+    /**
+     * Replaces a short assignment expression like 'left sign= right' where
+     * 'left' is the name of a protected variable.
+     * 
+     * @param left name of the variable on the left side
+     * @param right expression on the right side of assignment
+     * @param sign the sign of the assignment: '+', '-', '*', '/', '&amp;',
+     *        '|', '%', '&lt;&lt;&lt;', '&lt;&lt;' or '&gt;&gt;' 
+     * @param var the variable from the left side
+     * @param tree whole assignment tree
+     */
     public void setAssign(String left, String right, String sign, Variable var, CommonTree tree) {
         if (!var.shouldBeTransformed()) {
             return;
@@ -115,6 +153,13 @@ public abstract class IntegrityVariablesMembers extends AbstractTreeParser {
         replaceTree(st.toString(), tree);
     }
 
+    /**
+     * Replaces simple assignment expression (only if variable is protected).
+     * 
+     * @param expression expression on the right side of the assignment
+     * @param var the variable
+     * @param tree only the right side of the assignment
+     */
     public void setResistantType(String expression, Variable var, CommonTree tree) {
         if (!var.shouldBeTransformed()) {
             return;
@@ -124,6 +169,13 @@ public abstract class IntegrityVariablesMembers extends AbstractTreeParser {
         replaceTree(st.toString(), tree);
     }
 
+    /**
+     * Replaces preincrementation or predecrementation of a protected variable.
+     * 
+     * @param var the variable which is incremented/decremented
+     * @param sign '+' for incrementation, '-' for decrementation
+     * @param tree whole incrementation/decrementation tree
+     */
     public void setPreIncDec(Variable var, String sign, CommonTree tree) {
         if (!var.shouldBeTransformed()) {
             return;
@@ -134,21 +186,33 @@ public abstract class IntegrityVariablesMembers extends AbstractTreeParser {
         replaceTree(st.toString(), tree);
     }
 
+    /**
+     * Replaces postincrementation or postdecrementation of a protected variable.
+     * 
+     * @param var the variable which is incremented/decremented
+     * @param sign '+' for incrementation, '-' for decrementation
+     * @param tree whole incrementation/decrementation tree
+     */
     public void setPostIncDec(Variable var, String sign, CommonTree tree) {
         if (!var.shouldBeTransformed()) {
             return;
         }
+        // TODO: handle situations like 'a = i++'
         StringTemplate st = getTemplateLib().getInstanceOf("unwrapPostIncrementation_" + var.getType());
         st.setAttribute("variable", var.getName());
         st.setAttribute("sign", sign);
         replaceTree(st.toString(), tree);
     }
 
+    /**
+     * Adds getter to get the original value of a protected variable.
+     * 
+     * @param expression full variable name (primary expression)
+     * @param var the variable
+     * @param tree primary expression with the variable
+     */
     public void getResistantType(String expression, Variable var, CommonTree tree) {
-        if (var == null) { // the identifier is not a variable
-            System.out.println("null variable: " + expression);
-            return;
-        }
+        assert var == null : "null variable"; 
         if (!var.shouldBeTransformed()) {
             return;
         }
@@ -157,6 +221,12 @@ public abstract class IntegrityVariablesMembers extends AbstractTreeParser {
         replaceTree(st.toString(), tree);
     }
 
+    /**
+     * Replaces the given tree with the given string.
+     * 
+     * @param str new content
+     * @param tree original tree
+     */
     public void replaceTree(String str, CommonTree tree) {
         tokens.replace(tree.getTokenStartIndex(), tree.getTokenStopIndex(), str);
     }
